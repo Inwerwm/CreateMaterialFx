f27783104f14b7517a9683644d12f56506a63bf7
Inwerwm <saclainvervm@gmail.com>
2021-12-04 21:28:37 +0900

    自動テストプロジェクトを追加

---
+++ UnitTest/MaterialGeneratorTest.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest;
+[TestClass]
+public class MaterialGeneratorTest
+{
+    [TestMethod]
+    public void TestGenerate()
+    {
+
+    }
+}
