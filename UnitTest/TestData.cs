b082300f91bee546a4c961ac6a8b36d6c6c16927
Inwerwm <saclainvervm@gmail.com>
2021-12-05 00:23:12 +0900

    テストデータフォルダの指定が間違っていたので修正

--- UnitTest/TestData.cs
+++ UnitTest/TestData.cs
@@ -2,7 +2,7 @@
 
 internal static class TestData
 {
-    public static string Directory { get; } = "../TestData";
+    public static string Directory { get; } = @"..\..\TestData";
 
     public static string Get(string filename) => Path.Combine(Directory, filename);
 }
