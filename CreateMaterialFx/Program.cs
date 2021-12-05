90988ef23ac1f6d68f0f9a2be5a2fd00490cf6e0
Inwerwm <saclainvervm@gmail.com>
2021-12-05 12:56:42 +0900

    上書きを起動引数で選べるようにした

--- CreateMaterialFx/Program.cs
+++ CreateMaterialFx/Program.cs
@@ -4,12 +4,15 @@ if (args.Length < 2)
 {
     Console.WriteLine("引数1: マップ画像フォルダのあるディレクトリのパス");
     Console.WriteLine("引数2: 書込み先ディレクトリのパス");
+    Console.WriteLine("引数3: 上書きするかの真理値 設定なしなら false");
     return;
 }
 
 var mapFolderPaths = Directory.EnumerateDirectories(args[0]);
 var fxes = mapFolderPaths.Select(fp => (Material: new SdPbrMaterial(fp, "../../../../_JulioSillet/Map", new()), Name: Path.GetFileName(fp)));
+bool.TryParse(args[2], out bool enableOverWrite);
+
 foreach (var fx in fxes)
 {
-    fx.Material.Write(Path.Join(args[1], $"{fx.Name}.fx"));
+    fx.Material.Write(Path.Join(args[1], $"{fx.Name}.fx"), enableOverWrite);
 }
