6d24d5414b48fa6e84c272fe029a46c11e0022d4
Inwerwm <saclainvervm@gmail.com>
2021-12-05 12:16:53 +0900

    起動用コンソールアプリケーションを作成

--- CreateMaterialFx/Program.cs
+++ CreateMaterialFx/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using MaterialGenerator.Material;
+
+if (args.Length < 2)
+{
+    Console.WriteLine("引数1: マップ画像フォルダのあるディレクトリのパス");
+    Console.WriteLine("引数2: 書込み先ディレクトリのパス");
+    return;
+}
+
+var mapFolderPaths = Directory.EnumerateDirectories(args[0]);
+var fxes = mapFolderPaths.Select(fp => (Material: new SdPbrMaterial(fp, "../../../../_JulioSillet/Map", new()), Name: Path.GetFileName(fp)));
+foreach (var fx in fxes)
+{
+    fx.Material.Write(Path.Join(args[1], $"{fx.Name}.fx"));
+}
